f9f324093606e25498a006a0010100572339996f
Jirawat Ruenanukul <amplifiers9@gmail.com>
2022-07-26 18:28:40 +0700

    1.2.7

--- src/dbs/general-equipments/cards.ts
+++ src/dbs/general-equipments/cards.ts
@@ -1638,4 +1638,40 @@ export const cards: IBaseEquipment[] = [
       }
     }
   },
+  {
+    id: 27103,
+    name: 'Living Dead Card',
+    type: EquipmentType.CARD,
+    subType: null,
+    location: null,
+    compoundOn: CompoundOn.ARMOR,
+    baseDef: 0,
+    baseATK: 0,
+    level: 1,
+    equipUpgradeValue: 0,
+    str: 0,
+    agi: 0,
+    vit: 0,
+    int: 0,
+    dex: 0,
+    luk: 0,
+    additionAtk: {
+      size: 0,
+      element: 0,
+      race: 0,
+      class: 0,
+    },
+    additionDef: {
+      size: 0,
+      element: 0,
+      race: 0,
+      class: 0,
+    },
+    eATK: 20,
+    cATK: 0,
+    criticalPercent: 0,
+    penetrationPercent: 0,
+    prefix: 'Vigor',
+    script: (character: Character2, _this: IBaseEquipment): void => { }
+  },
 ];
